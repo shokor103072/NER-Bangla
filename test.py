d19a6ce0f994f5cddf9285c23ea115f663b739d9
Shokor A Rahman <47053139+shokor103072@users.noreply.github.com>
2023-06-30 01:16:29 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,34 @@
 from bnlp import NER
-bn_ner = NER()
-model_path = r"C:\Users\HP\Desktop\NER_Bangla\model\ner_model.pkl"
-text = "ডিপিডিসির স্পেশাল টাস্কফোর্সের প্রধান মুনীর চৌধুরী জানান "
-result = bn_ner.tag(model_path, text)
-# Filter the results to only include 'Person' tag
-person_tags = [t for t in result if t[1] == 'B-PER' or t[1] == 'E-PER' or t[1] == 'S-PER']
 
-print(person_tags)
-print(result)
+def collect_entities(tagged_text):
+    entities = []
+    current_entity = []
+    for word, tag in tagged_text:
+        if tag == 'O':
+            if current_entity:
+                entities.append(' '.join(current_entity))
+                current_entity = []
+        elif tag in ['B-PER', 'I-PER', 'E-PER', 'S-PER']:
+            current_entity.append(word)
+    if current_entity:
+        entities.append(' '.join(current_entity))
+    return entities
+
+# Load trained model
+bn_ner = NER()
+model_path = "./ner_model.pkl"
+
+# List of texts for Named Entity Recognition
+text_list = [
+    "আব্দুর রহিম নামের কাস্টমারকে একশ টাকা বাকি দিলাম",
+    "১০০ টাকা জমা দিয়েছেন কবির",
+    "ডিপিডিসির স্পেশাল টাস্কফোর্সের প্রধান মুনীর চৌধুরী জানান",
+    "অগ্রণী ব্যাংকের জ্যেষ্ঠ কর্মকর্তা পদে নিয়োগ পরীক্ষার প্রশ্নপত্র ফাঁসের অভিযোগ উঠেছে।",
+    "সে আজকে ঢাকা যাবে",
+]
+
+# Perform Named Entity Recognition for each text and extract entities tagged as 'Person'
+for text in text_list:
+    tagged_text = bn_ner.tag(model_path, text)
+    person_entities = collect_entities(tagged_text)
+    print(person_entities)
